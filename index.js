d5a204696dec2e34d11e8c48e549a7b38316690d
Amar AA <amar.ashfaq95@gmail.com>
2024-03-03 22:38:11 +0000

    The nav links are now interactive - can now tell which link has been clicked

--- index.js
+++ index.js
@@ -9,25 +9,62 @@ const navBar = document.querySelector("#navbar");
 const projectsSection = document.querySelector("#projects");
 const offset = projectsSection.offsetTop - navBar.offsetHeight;
 
+const navLinks = document.querySelectorAll("#navbar a");
+
 introResumeBtn.addEventListener("click", () => {
     introResumeBtn.href = pdfUrl;
     introResumeBtn.target = '_blank';
 });
 
 document.addEventListener("DOMContentLoaded", () => {
-    projectsBtn.style.color = "#827E7E";
-    contactBtn.style.color = "#827E7E";
+    aboutBtn.style.color = "#1E1E1E";
 
-    AlternateNavBarBackgroundColour();
+    window.scrollTo({
+        top: 0
+    });
 });
 
-function AlternateNavBarBackgroundColour(){
-    window.addEventListener("scroll", () => {
-        if (window.scrollY > offset) {
-            navBar.style.backgroundColor = "#FFFFFF"; // Change to your desired background color
+window.addEventListener("scroll", () => {
+    if (window.scrollY > offset) {
+        navBar.style.backgroundColor = "#FFFFFF"; // Change to your desired background color
+    } 
+    else {
+        navBar.style.backgroundColor = "#F2F7F9";
+    }
+});
+
+navLinks.forEach((navLink) => {
+    navLink.addEventListener("click", (event) => {
+        event.preventDefault();
+        const targetId = event.currentTarget.getAttribute("href").substring(1);
+        const targetElement = document.getElementById(targetId);
+
+      if (targetElement) {
+
+        let scrollToPosition = targetElement.offsetTop - navBar.offsetHeight;
+
+        if (targetId === "contact") {
+            console.log("contact has been clicked...")
+          // If the link is for "Contact", navigate to the new page
+          //window.location.href = this.getAttribute("href");       
         } 
         else {
-            navBar.style.backgroundColor = "#F2F7F9";
+            // For other links, scroll to the target element
+            window.scrollTo({
+                top: scrollToPosition,
+                behavior: "smooth"
+            });
+
+            ResetFontColoursOfNavLinks()
+            navLink.style.color = "#1E1E1E"
         }
+      }
+    });
+});
+
+function ResetFontColoursOfNavLinks() {
+    // Reset the font color for all links
+    navLinks.forEach(function(navLink) {
+        navLink.style.color = "#827E7E";
     });
 }
