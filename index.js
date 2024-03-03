7dd33926b5ad4ece55eb02e36cdab12109747b84
Amar AA <amar.ashfaq95@gmail.com>
2024-03-03 00:49:00 +0000

    Applied alternating background colour to the navbar when scrolling past the projects section and vice-versa. A transition was added to create a smooth effect

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const aboutBtn = document.querySelector("#aboutBtn");
 const projectsBtn = document.querySelector("#projectsBtn");
 const contactBtn = document.querySelector("#contactBtn");
 
+const navBar = document.querySelector("#navbar");
+const projectsSection = document.querySelector("#projects");
+const offset = projectsSection.offsetTop - navBar.offsetHeight;
+
 introResumeBtn.addEventListener("click", () => {
     introResumeBtn.href = pdfUrl;
     introResumeBtn.target = '_blank';
@@ -13,4 +17,17 @@ introResumeBtn.addEventListener("click", () => {
 document.addEventListener("DOMContentLoaded", () => {
     projectsBtn.style.color = "#827E7E";
     contactBtn.style.color = "#827E7E";
+
+    AlternateNavBarBackgroundColour();
 });
+
+function AlternateNavBarBackgroundColour(){
+    window.addEventListener("scroll", () => {
+        if (window.scrollY > offset) {
+            navBar.style.backgroundColor = "#FFFFFF"; // Change to your desired background color
+        } 
+        else {
+            navBar.style.backgroundColor = "#F2F7F9";
+        }
+    });
+}
