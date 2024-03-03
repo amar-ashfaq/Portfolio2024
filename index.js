df98bac7750a83dd177bd739e0ce03772e85f03a
Amar AA <amar.ashfaq95@gmail.com>
2024-03-03 00:21:32 +0000

    Added some JavaScript to fix the font colors of the nav links

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 const introResumeBtn = document.querySelector("#intro-resume-btn");
 const pdfUrl = "https://drive.google.com/file/d/1q59QdqEgS8nzf2KUEW5HRiooZbjfKNgC/view?usp=sharing";
 
+const aboutBtn = document.querySelector("#aboutBtn");
+const projectsBtn = document.querySelector("#projectsBtn");
+const contactBtn = document.querySelector("#contactBtn");
+
 introResumeBtn.addEventListener("click", () => {
     introResumeBtn.href = pdfUrl;
     introResumeBtn.target = '_blank';
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+    projectsBtn.style.color = "#827E7E";
+    contactBtn.style.color = "#827E7E";
 });
