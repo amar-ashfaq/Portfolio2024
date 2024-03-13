06e640fdfbd3d369bc015d869be94fcb6ae650df
Amar AA <amar.ashfaq95@gmail.com>
2024-03-13 00:26:15 +0000

    Implemented the sidebar feature for the navigation bar. This will allow navbar functionality for mobile devices. Will require some tidying up down the line

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const introResumeBtn = document.querySelector("#intro-resume-btn");
 const pdfUrl = "https://drive.google.com/file/d/1ET0tNWFwdehFGPtzdFRU2gh4g8lmJCIg/view?usp=sharing";
 
-const aboutBtn = document.querySelector("#aboutBtn");
-const projectsBtn = document.querySelector("#projectsBtn");
-const contactBtn = document.querySelector("#contactBtn");
+const aboutBtn = document.querySelector("#aboutBtn1");
+const projectsBtn = document.querySelector("#projectsBtn1");
+const contactBtn = document.querySelector("#contactBtn1");
 
 const navBar = document.querySelector("#navbar");
 const projectsSection = document.querySelector("#projects");
@@ -11,18 +11,59 @@ const offset = projectsSection.offsetTop - navBar.offsetHeight;
 
 const navLinks = document.querySelectorAll(".navLink");
 
+const burgerMenu = document.getElementById('burgerMenu');
+const mobileLinks = document.getElementById('mobileLinks');
+const closeBtn = document.getElementById('closeBtn');
+
+
+// tidy up everything inside here - todo
+
+
 introResumeBtn.addEventListener("click", () => {
     introResumeBtn.href = pdfUrl;
     introResumeBtn.target = '_blank';
 });
 
 document.addEventListener("DOMContentLoaded", () => {
-    aboutBtn.style.color = "#1E1E1E";
+    document.querySelector("#aboutBtn").style.color = "#1E1E1E";
 
     window.scrollTo({
         top: 0
     });
 });
+
+document.addEventListener('DOMContentLoaded', function () {
+    
+    burgerMenu.addEventListener('click', function () {
+        mobileLinks.classList.add('show');
+    });
+
+    closeBtn.addEventListener('click', function () {
+        mobileLinks.classList.remove('show');
+    });
+
+    aboutBtn.addEventListener('click', function (event) {
+        event.preventDefault(); // Prevent default anchor behavior
+        scrollToTarget('about');
+        // Close the sidebar
+        mobileLinks.classList.remove('show');
+    });
+
+    projectsBtn.addEventListener('click', function (event) {
+        event.preventDefault(); // Prevent default anchor behavior
+        scrollToTarget('projects');
+        // Close the sidebar
+        mobileLinks.classList.remove('show');
+    });
+});
+
+function scrollToTarget(targetId) {
+    const targetElement = document.getElementById(targetId);
+    if (targetElement) {
+      let scrollToPosition = targetElement.offsetTop - navBar.offsetHeight;
+      window.scrollTo({ top: scrollToPosition, behavior: 'smooth' });
+    }
+}
 
 window.addEventListener("scroll", () => {
     if (window.scrollY > offset) {
@@ -32,39 +73,3 @@ window.addEventListener("scroll", () => {
         navBar.style.backgroundColor = "#F2F7F9";
     }
 });
-
-navLinks.forEach((navLink) => {
-    navLink.addEventListener("click", (event) => {
-        event.preventDefault();
-        const targetId = event.currentTarget.getAttribute("href").substring(1);
-        const targetElement = document.getElementById(targetId);
-
-      if (targetElement) {
-
-        let scrollToPosition = targetElement.offsetTop - navBar.offsetHeight;
-
-        if (targetId === "contact") {
-            console.log("contact has been clicked...")
-          // If the link is for "Contact", navigate to the new page
-          //window.location.href = this.getAttribute("href");       
-        } 
-        else {
-            // For other links, scroll to the target element
-            window.scrollTo({
-                top: scrollToPosition,
-                behavior: "smooth"
-            });
-
-            ResetFontColoursOfNavLinks()
-            navLink.style.color = "#1E1E1E"
-        }
-      }
-    });
-});
-
-function ResetFontColoursOfNavLinks() {
-    // Reset the font color for all links
-    navLinks.forEach(function(navLink) {
-        navLink.style.color = "#827E7E";
-    });
-}
